d7e22631a631c038a4acf22cb395765466a0ba8d
Luiz-Eddy <luizeduardo-catarino@outlook.com>
2021-05-23 22:36:12 -0300

    Melhor descrição da função include JS

--- js/script.js
+++ js/script.js
@@ -34,27 +34,27 @@ function oc(){
 // Includes HTML
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
+    /*percorre pelo html pegando todos os seletores:*/
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
       elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
+      /*pesquisa pelo elemento com o atributo abaixo:*/
       file = elmnt.getAttribute("include");
       if (file) {
-        /*make an HTTP request using the attribute value as the file name:*/
+        /*abre uma solicitação HTTP para usar o conteúdo do atributo como nome da página solicitada:*/
         xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState == 4) {
             if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /*remove the attribute, and call this function once more:*/
+            if (this.status == 404) {elmnt.innerHTML = "Opa! Erro em include.";}
+            /*remove o atributo e chama a função novamente:*/
             elmnt.removeAttribute("include");
             includeHTML();
           }
         }      
         xhttp.open("GET", file, true);
         xhttp.send();
-        /*exit the function:*/
+        /*saída da função:*/
         return;
       }
     }
